8d40c252f772246f30098fd22c6bdf614f251cb1
Harshitha <146416736+Harshitha-Saravanan@users.noreply.github.com>
2025-01-10 18:36:33 +0530

    Update AirlineNotFound.js

--- Flight-inAir-master/src/exceptions/AirlineNotFound.js
+++ Flight-inAir-master/src/exceptions/AirlineNotFound.js
@@ -1,1 +1,9 @@
+class AirlineNotFoundError extends Error {
+    constructor(message) {
+        super(message); 
+        this.name = this.constructor.name; 
+        this.statusCode = 404; 
+    }
+}
 
+module.exports = AirlineNotFoundError;
