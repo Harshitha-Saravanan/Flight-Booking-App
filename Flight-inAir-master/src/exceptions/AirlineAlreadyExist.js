e7e1908e3ac207e6b42b3e052b7ac233f95fedf6
Harshitha <146416736+Harshitha-Saravanan@users.noreply.github.com>
2025-01-10 18:34:54 +0530

    Update AirlineAlreadyExist.js

--- Flight-inAir-master/src/exceptions/AirlineAlreadyExist.js
+++ Flight-inAir-master/src/exceptions/AirlineAlreadyExist.js
@@ -1,1 +1,9 @@
+class AirlineAlreadyExistsError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = this.constructor.name;
+        this.statusCode = 400;  
+    }
+}
 
+module.exports = AirlineAlreadyExistsError;
