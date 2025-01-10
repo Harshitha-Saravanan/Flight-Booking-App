bb4003b85aaa289b77271ac8484a630100917e50
Harshitha <146416736+Harshitha-Saravanan@users.noreply.github.com>
2025-01-10 13:37:22 +0530

    Update AirlineAlreadyExist.js

--- Flight-inAir-master/src/exceptions/AirlineAlreadyExist.java
+++ Flight-inAir-master/src/exceptions/AirlineAlreadyExist.java
@@ -1,10 +1,9 @@
-package exceptions;
-
-public class AirlineAlreadyExist extends Exception {
-    public AirlineAlreadyExist() {
-    }
-
-    public AirlineAlreadyExist(String message) {
+class AirlineAlreadyExistsError extends Error {
+    constructor(message) {
         super(message);
+        this.name = this.constructor.name;
+        this.statusCode = 400;  // Set an appropriate HTTP status code (400: Bad Request)
     }
 }
+
+module.exports = AirlineAlreadyExistsError;
