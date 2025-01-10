daca248706fba2cb6b5b9b5a4f86e1b8fd0b0a9a
Harshitha <146416736+Harshitha-Saravanan@users.noreply.github.com>
2025-01-10 18:43:32 +0530

    Update Flight.js

--- Flight-inAir-master/src/models/Flight.js
+++ Flight-inAir-master/src/models/Flight.js
@@ -1,1 +1,23 @@
+const Flight = require('./Flight');
+const Airline = require('./Airline'); 
 
+
+const airline = new Airline('Jet Airways');
+
+
+const flight = new Flight('Flight101', 'New York', 'London', 500, airline);
+
+
+flight.addService('Meal');
+flight.addService('WiFi');
+
+
+console.log(flight.getName()); 
+console.log(flight.getServices()); 
+console.log(flight.toString()); 
+
+const flight2 = new Flight('Flight101', 'New York', 'London', 500, airline);
+console.log(flight.equals(flight2)); 
+
+
+console.log(flight.hashCode()); 
