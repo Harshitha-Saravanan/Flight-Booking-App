eeb68faa8e57a81bff2bac3d91f124a7ecf8db84
Harshitha <146416736+Harshitha-Saravanan@users.noreply.github.com>
2025-01-10 18:37:18 +0530

    Delete Flight-inAir-master/src/exceptions/FlightAlreadyExist.java

--- Flight-inAir-master/src/exceptions/FlightAlreadyExist.java
+++
@@ -1,9 +0,0 @@
-class FlightAlreadyExistError extends Error {
-    constructor(message) {
-        super(message); 
-        this.name = this.constructor.name; 
-        this.statusCode = 409; 
-    }
-}
-
-module.exports = FlightAlreadyExistError;
