72132ecf45e98fc3985db29e7243177920c6a17e
Harshitha <146416736+Harshitha-Saravanan@users.noreply.github.com>
2025-01-10 18:40:57 +0530

    Update Airline.js

--- Flight-inAir-master/src/models/Airline.js
+++ Flight-inAir-master/src/models/Airline.js
@@ -1,1 +1,23 @@
+const Airline = require('./Airline'); 
 
+
+const flight1 = { flightNumber: 'A101', destination: 'NYC' };
+const flight2 = { flightNumber: 'A102', destination: 'LA' };
+
+
+const airline = new Airline('Jet Airways');
+
+
+airline.addFlight(flight1);
+airline.addFlight(flight2);
+
+
+
+
+console.log(airline.getFlights()); 
+
+const airline2 = new Airline('Jet Airways');
+console.log(airline.equals(airline2)); 
+
+
+console.log(airline.hashCode()); 
