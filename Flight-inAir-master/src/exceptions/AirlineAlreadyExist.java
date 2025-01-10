c2c2fd80272a92707bb2b3549b274e9633920dcd
Harshitha <146416736+Harshitha-Saravanan@users.noreply.github.com>
2025-01-10 18:33:50 +0530

    Delete Flight-inAir-master/src/exceptions/AirlineAlreadyExist.java

--- Flight-inAir-master/src/exceptions/AirlineAlreadyExist.java
+++
@@ -1,9 +0,0 @@
-class AirlineAlreadyExistsError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = this.constructor.name;
-        this.statusCode = 400;  
-    }
-}
-
-module.exports = AirlineAlreadyExistsError;
