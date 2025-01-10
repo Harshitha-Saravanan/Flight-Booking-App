a398ebe8cd6a0890c9a6a86be5a129e97b78f95f
Harshitha <146416736+Harshitha-Saravanan@users.noreply.github.com>
2025-01-10 18:35:48 +0530

    Delete Flight-inAir-master/src/exceptions/AirlineNotFound.java

--- Flight-inAir-master/src/exceptions/AirlineNotFound.java
+++
@@ -1,9 +0,0 @@
-class AirlineNotFoundError extends Error {
-    constructor(message) {
-        super(message); 
-        this.name = this.constructor.name; 
-        this.statusCode = 404; 
-    }
-}
-
-module.exports = AirlineNotFoundError;
