eb638fc3d36854bcf43c2712cabaf5f0a1f9207f
Harshitha <146416736+Harshitha-Saravanan@users.noreply.github.com>
2025-01-10 18:39:32 +0530

    Update FlightNotFound.js

--- Flight-inAir-master/src/exceptions/FlightNotFound.js
+++ Flight-inAir-master/src/exceptions/FlightNotFound.js
@@ -1,1 +1,9 @@
+class FlightNotFoundError extends Error {
+    constructor(message) {
+        super(message); 
+        this.name = this.constructor.name; 
+        this.statusCode = 404; 
+    }
+}
 
+module.exports = FlightNotFoundError;
