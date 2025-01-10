9f72861711846d857a0f21d87b1e243e42ddef21
Harshitha <146416736+Harshitha-Saravanan@users.noreply.github.com>
2025-01-10 18:38:36 +0530

    Delete Flight-inAir-master/src/exceptions/FlightNotFound.java

--- Flight-inAir-master/src/exceptions/FlightNotFound.java
+++
@@ -1,9 +0,0 @@
-class FlightNotFoundError extends Error {
-    constructor(message) {
-        super(message); // Call the parent Error constructor with the message
-        this.name = this.constructor.name; // Set the error name to the class name
-        this.statusCode = 404; // HTTP status code for "Not Found"
-    }
-}
-
-module.exports = FlightNotFoundError;
