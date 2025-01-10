46c9a70360b7b2c4534f788073fe3b3c9dc45a81
Harshitha <146416736+Harshitha-Saravanan@users.noreply.github.com>
2025-01-10 13:53:45 +0530

    Update Flight.js

--- Flight-inAir-master/src/models/Flight.java
+++ Flight-inAir-master/src/models/Flight.java
@@ -1,80 +1,23 @@
-package models;
+const Flight = require('./Flight');
+const Airline = require('./Airline'); 
 
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
 
-public class Flight extends BaseModel {
-    private final String name;
-    private final String origin;
-    private final String destination;
-    private final double fare;
-    private final Airline airline;
-    private final Set<String> services;
+const airline = new Airline('Jet Airways');
 
-    public Flight(String name, String origin, String destination, double fare, Airline airline) {
-        this.name = name;
-        this.origin = origin;
-        this.destination = destination;
-        this.fare = fare;
-        this.airline = airline;
-        this.services = new HashSet<>();
-    }
 
-    public String getName() {
-        return name;
-    }
+const flight = new Flight('Flight101', 'New York', 'London', 500, airline);
 
-    public String getOrigin() {
-        return origin;
-    }
 
-    public String getDestination() {
-        return destination;
-    }
+flight.addService('Meal');
+flight.addService('WiFi');
 
-    public double getFare() {
-        return fare;
-    }
 
-    public Set<String> getService() {
-        return services;
-    }
+console.log(flight.getName()); 
+console.log(flight.getServices()); 
+console.log(flight.toString()); 
 
-    public void addService(String service) {
-        services.add(service);
-    }
+const flight2 = new Flight('Flight101', 'New York', 'London', 500, airline);
+console.log(flight.equals(flight2)); 
 
-    public Airline getAirline() {
-        return airline;
-    }
 
-    public Set<String> getServices() {
-        return services;
-    }
-
-    @Override
-    public String toString() {
-        return "Flight{" +
-                "name='" + name + '\'' +
-                ", origin='" + origin + '\'' +
-                ", destination='" + destination + '\'' +
-                ", fare=" + fare +
-                ", airline=" + airline +
-                ", services=" + services +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Flight flight = (Flight) o;
-        return Double.compare(fare, flight.fare) == 0 && Objects.equals(name, flight.name) && Objects.equals(origin, flight.origin) && Objects.equals(destination, flight.destination) && airline.equals(flight.getAirline());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, origin, destination, fare, airline);
-    }
-}
+console.log(flight.hashCode()); 
