30a0851fb96697ffb264ca4fe7baaecc02642f42
Harshitha <146416736+Harshitha-Saravanan@users.noreply.github.com>
2025-01-10 18:42:09 +0530

    Update BaseModel.js

--- Flight-inAir-master/src/models/BaseModel.js
+++ Flight-inAir-master/src/models/BaseModel.js
@@ -1,1 +1,17 @@
+class BaseModel {
+    constructor(id) {
+        this._id = id || null; 
+    }
 
+    
+    getId() {
+        return this._id;
+    }
+
+    
+    setId(id) {
+        this._id = id;
+    }
+}
+
+module.exports = BaseModel;
